f1800fb7d621596ad13632eaa372bd67ca6d6d17
crbhf1016 <1584545294@qq.com>
2020-10-23 20:16:07 +0800

    超人再次提交

--- itstack-demo-design-1-00/src/main/java/org/itstack/demo/design/card/IQiYiCard.java
+++ itstack-demo-design-1-00/src/main/java/org/itstack/demo/design/card/IQiYiCard.java
@@ -3,10 +3,11 @@ package org.itstack.demo.design.card;
 public class IQiYiCard {
 
     public static void main(String[] args) {
-        System.out.println("超人分支");
+        System.out.println("超人分支客户端");
 
 
-        System.out.println("超人再次提交");
+        System.out.println("超人再次提交客户端");
     }
+
 
 }
