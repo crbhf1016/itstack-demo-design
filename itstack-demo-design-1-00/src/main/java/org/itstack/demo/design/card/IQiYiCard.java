2da83bd4c8564a90b11ec50837671deb399fa327
crbhf1016 <31908574+crbhf1016@users.noreply.github.com>
2020-10-23 20:15:18 +0800

    Update IQiYiCard.java

--- itstack-demo-design-1-00/src/main/java/org/itstack/demo/design/card/IQiYiCard.java
+++ itstack-demo-design-1-00/src/main/java/org/itstack/demo/design/card/IQiYiCard.java
@@ -3,7 +3,7 @@ package org.itstack.demo.design.card;
 public class IQiYiCard {
 
     public static void main(String[] args) {
-        System.out.println("超人分支");
+        System.out.println("超人分支服务器");
 
 
         System.out.println("超人再次提交");
